2701e552d045a6932af11dd707f33fe5dd78f30b
Aiden Lee <30446886+adaitor3@users.noreply.github.com>
2023-01-08 14:16:37 -0600

    Add files via upload

---
+++ EmployeeSQL/Table Schema.sql
@@ -0,0 +1,40 @@
+﻿CREATE TABLE titles(
+    title_id VARCHAR PRIMARY KEY,
+    title VARCHAR NOT NULL
+);
+
+CREATE TABLE employees(
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex VARCHAR NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE salaries(
+    emp_no INT NOT NULL,
+    salary MONEY NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+); 
+
+CREATE TABLE departments(
+    dept_no VARCHAR PRIMARY KEY,
+    dept_name VARCHAR NOT NULL
+);
+
+CREATE TABLE dept_manager(
+    dept_no VARCHAR NOT NULL,
+    emp_no INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_emp(
+    emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+    );
+
